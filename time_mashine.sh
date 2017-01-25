636d795f72d8cb8dc30300fdf35e9d8bebb1939e
iop <iop@bed.xz>
2017-01-25 20:02:32 +0300

    протестированный скрипрт, выполняющий свою основную функцию есть небольшой "HELP"

--- time_mashine.sh
+++ time_mashine.sh
@@ -1,16 +1,38 @@
 #!/usr/bin/env bash
 
+## Резервное архивирование (backup) всех файлов в заданом каталоге,
+## которые были изменены с момента последнего саздания backup
+## Файлы складываются в инкрементный архив
+
+## Writen by: Danilov Paul - danilov.paul@gmail.com on 01-2017
+
 #TODO: добавить зпись в Backup.log
 #TODO: backup.log <-- начало и окончание процесса; объём переданных данных
 #TODO: добавить проверку свободного дискового пространства
 
-
 function checkOption { 				#проверка на наличие входящего параметра
-	if [ -z "${1-}" ]; then
+	if [ -z "${1:-}" ]; then
 	   #echo "Must provide var environment variable. Exiting...."
 	   echo "$0: missing operand"
 	   exit 1
 	fi
+}
+
+function print_help {
+	default_dest="${1:-/mnt/backup}"
+
+	echo "$(basename $0) based RSYNC file transfer program"
+	echo
+	echo "$(basename $0) comes with ABSOLUTELY NO WARRANTY.  This is free software, and you"
+	echo "are welcome to redistribute it under certain conditions.  See the GNU"
+	echo "General Public Licence for details."
+	echo
+	echo "$(basename $0) transfer local dir to local storage on the mounted section section"
+	echo "default backup destination:"
+	echo "		${default_dest}"
+	echo
+	echo "Usage: $(basename $0) SRC_BACKUP_DIR [DEST_BACKUP_DIR]"
+	echo
 }
 
 function checkSouchPath {
@@ -45,11 +67,17 @@ function rmlink {
 }
 
 function updatelink {
-	rmlink "$1"
-	foo=`pwd`
-	cd "$PREFBCK"
-	ln -sf "$DATE" "$LNKDST"
-	cd "$foo"
+#	echo "BACKUP in: $1"
+#	если есть символическая ссылка на каталог, то она !удаляется
+	[ -L "$1/${LNKDST}" ] && rm -f "$1/${LNKDST}"
+	foo=$(pwd)
+#	echo -n "$(pwd) -> "
+	cd "$1"
+	#echo "make lnk from ${DATE} as ${LNKDST}"
+	[ -d "${DATE}" ] && echo "set link new dest" && ln -sf "${DATE}" "$LNKDST"
+	#ls -lha
+	cd "${foo}"
+
 }
 
 function backupLog {
@@ -62,39 +90,59 @@ function checkSize {
 	echo "Space needed for this backup: $CHECK" >> $2
 }
 
-checkOption $1
-checkSouchPath $1
+die () {
+	# убийство выполнения скрипта
+	echo "FATAL ERROR: $* (status $?)"  1>&2
+	exit 1
+}
+
+
+	# проверка входных аргументов
+if [ -z "$1" ]
+then
+	print_help #"/mnt/backup/Backups.backupdb"
+	# echo $? --  код завершения сценария
+	exit 65
+fi
 
 # установка основных путей
-#BACKUPDIR="/mnt/backup/Backups.backupdb"
-BACKUPDIR="Backups.backupdb"
+SRC="$1"
+[ -z "$2" ] && bckp_pref="Backups.backupdb"
+#BACKUPDIR=${2:-"Backups.backupdb"} # так тоже можно
+
 BACKUPNAME="$( basename $1 )"
+
 BACKSUFF="bckp"
 
-PREFBCK="$BACKUPDIR/$BACKUPNAME.$BACKSUFF"
+checkSouchPath $1
+
+
+backup_dest="${bckp_pref}/${BACKUPNAME}.${BACKSUFF}" # установка MAIN каталога для размещения копии файлов
 
 DATE=`date +%F.%H%M%S`
 
-BCK="$PREFBCK/$DATE/"
+cur_backup_dest="${backup_dest}/${DATE}" 				#  каталог для размещения будующего backup
+
 LNKDST="Latest"
 
 
 #checkmountpoint "$BACKUPDIR"
 
-mkNoSouchDir "$PREFBCK/$DATE"
-
-BACKUPFILELOG="$PREFBCK/$DATE/.Backup.log"
+#mkNoSouchDir "${PREFBCK}/${DATE}"
+mkNoSouchDir "${cur_backup_dest}"
+BACKUPFILELOG="${cur_backup_dest}/.Backup.log"
 
 backupLog "$BACKUPFILELOG"
 checkSize "$1" "$BACKUPFILELOG"
 
-# ------  синхронизация катлогов !!!
+echo "root dir: ${backup_dest}"
 
-rsync -av --link-dest="$PWD/$PREFBCK/$LNKDST/" "$1" "$BCK" --log-file="$BACKUPFILELOG" --delete
+# ------  синхронизация катлогов !!!
+rsync -a --link-dest="$PWD/${backup_dest}/$LNKDST" "$1" "${cur_backup_dest}/" --log-file="$BACKUPFILELOG" --delete
 
 # ------ финал: замена цели в simlink
 
-updatelink "$PREFBCK/$LNKDST"
+updatelink "$PWD/${backup_dest}"
 
 #echo "LINKDST: $PREFBCK/$LNKDST"
 #echo "DST: $PREFBCK/$DATE"
